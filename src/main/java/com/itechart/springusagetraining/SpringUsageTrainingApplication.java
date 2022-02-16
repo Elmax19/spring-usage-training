0a1e99a44bade163aa3db6cf2e73017b842ab355
Elmax19 <Maksim.Kakhnovich@mail.ru>
2022-02-16 09:30:58 +0300

    creating app

---
+++ src/main/java/com/itechart/springusagetraining/SpringUsageTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.itechart.springusagetraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUsageTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringUsageTrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itechart/springusagetraining/SpringUsageTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itechart.springusagetraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUsageTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
