aadcf16ebc19542c5038d61b158e3af5f3758692
Elmax19 <maksim.kohnovich@mail.ru>
2022-02-18 12:55:28 +0300

    rewriting ApplicationContextProvider

--- src/main/java/by/elmax19/app/context/ApplicationContextProvider.java
+++ src/main/java/by/elmax19/app/context/ApplicationContextProvider.java
@@ -1,27 +1,12 @@
 package by.elmax19.app.context;
 
-import org.springframework.beans.BeansException;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
-import org.springframework.stereotype.Component;
 
-@Component
-public class ApplicationContextProvider implements ApplicationContextAware {
-    private ApplicationContext applicationContext;
-
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        this.applicationContext = applicationContext;
-    }
-
-    @Bean
-    public ApplicationContext applicationContext() {
-        return this.applicationContext;
-    }
-
+@Configuration
+public class ApplicationContextProvider {
     @Bean
     public ClassPathXmlApplicationContext classPathContext() {
         return new ClassPathXmlApplicationContext("classPathContext.xml");
