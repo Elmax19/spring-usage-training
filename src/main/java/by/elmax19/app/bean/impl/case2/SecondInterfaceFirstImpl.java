7e30839e270509a73dda6e7f119546bbddad324d
Elmax19 <67223571+Elmax19@users.noreply.github.com>
2022-02-22 14:59:56 +0300

    Revert "Cases of possible beans resolving (#8)"
    This reverts commit 2faa844019bff7e3c3d71ad8d7a5dea78299365f.

--- src/main/java/by/elmax19/app/bean/impl/case2/SecondInterfaceFirstImpl.java
+++
@@ -1,9 +0,0 @@
-package by.elmax19.app.bean.impl.case2;
-
-import by.elmax19.app.bean.SecondInterface;
-import org.springframework.stereotype.Component;
-
-@Component("firstBean")
-public class SecondInterfaceFirstImpl implements SecondInterface {
-
-}
--- src/main/java/by/elmax19/app/bean/impl/case3/ThirdInterfaceFirstImpl.java
+++
@@ -1,9 +0,0 @@
-package by.elmax19.app.bean.impl.case3;
-
-import by.elmax19.app.bean.ThirdInterface;
-import org.springframework.stereotype.Component;
-
-@Component
-public class ThirdInterfaceFirstImpl implements ThirdInterface {
-
-}
