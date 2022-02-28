d69aba3331b31eaefd1051cb7c7f6f46410e8215
Elmax19 <maksim.kohnovich@mail.ru>
2022-02-28 12:38:11 +0300

    code refactoring and adding mocks in tests

--- src/main/java/by/elmax19/app/annotation/CustomOnPropertyCondition.java
+++ src/main/java/by/elmax19/app/annotation/CustomOnPropertyCondition.java
@@ -2,31 +2,31 @@ package by.elmax19.app.annotation;
 
 import org.springframework.context.annotation.Condition;
 import org.springframework.context.annotation.ConditionContext;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
 import org.springframework.core.env.PropertyResolver;
 import org.springframework.core.type.AnnotatedTypeMetadata;
 import org.springframework.util.MultiValueMap;
 
 import java.util.Objects;
 
-@Order(Ordered.HIGHEST_PRECEDENCE)
 public class CustomOnPropertyCondition implements Condition {
     @Override
     public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
         MultiValueMap<String, Object> map = metadata.getAllAnnotationAttributes(CustomConditionalOnProperty.class.getName());
         assert map != null;
         Property property = new Property(String.valueOf(map.get("name").get(0)), String.valueOf(map.get("value").get(0)));
+        if (property.getName().isBlank() || property.getValue().isBlank()) {
+            return false;
+        }
         PropertyResolver resolver = context.getEnvironment();
-        if ((map.containsKey("name"))) {
+        if (map.containsKey("name")) {
             return Objects.requireNonNull(resolver.getProperty(property.getName())).equalsIgnoreCase(property.getValue());
         }
         return false;
     }
 
     private class Property {
-        private String name = "";
-        private String value = "";
+        private final String name;
+        private final String value;
 
         public Property(String name, String value) {
             this.name = name;
--- src/test/java/by/elmax19/app/CustomConditionalOnPropertyTest.java
+++ src/test/java/by/elmax19/app/CustomConditionalOnPropertyTest.java
@@ -3,24 +3,32 @@ package by.elmax19.app;
 import by.elmax19.app.bean.CustomOnPropertyInterface;
 import by.elmax19.app.bean.impl.property.custom.FirstCustomOnPropertyImpl;
 import by.elmax19.app.bean.impl.property.custom.SecondCustomOnPropertyImpl;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.TestPropertySource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @TestPropertySource(properties = {"property.condition.selected.class = first"})
 public class CustomConditionalOnPropertyTest {
-    @Autowired
+    @Mock
     private CustomOnPropertyInterface bean;
-    @Autowired
+    @Mock
     private ApplicationContext context;
+
+    @BeforeAll
+    static void init() {
+        MockitoAnnotations.openMocks(CustomConditionalOnPropertyTest.class);
+    }
 
     @Test
     @DisplayName("First bean has been injected")
@@ -31,6 +39,7 @@ public class CustomConditionalOnPropertyTest {
     @Test
     @DisplayName("Second bean has not been injected")
     void checkSecondBeanAbsence() {
+        when(context.getBean(SecondCustomOnPropertyImpl.class)).thenThrow(NoSuchBeanDefinitionException.class);
         assertThrows(NoSuchBeanDefinitionException.class, () -> context.getBean(SecondCustomOnPropertyImpl.class));
     }
 }
