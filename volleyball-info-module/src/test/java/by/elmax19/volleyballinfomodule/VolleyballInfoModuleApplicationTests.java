32951a341e69a69dab49cf4f34933e54c65fad0a
Elmax19 <67223571+Elmax19@users.noreply.github.com>
2022-03-03 16:22:45 +0300

    creating multi-module project (#18)

---
+++ volleyball-info-module/src/test/java/by/elmax19/volleyballinfomodule/VolleyballInfoModuleApplicationTests.java
@@ -0,0 +1,14 @@
+package by.elmax19.volleyballinfomodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolleyballInfoModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("Loading VolleyballInfoModule context.");
+    }
+
+}
