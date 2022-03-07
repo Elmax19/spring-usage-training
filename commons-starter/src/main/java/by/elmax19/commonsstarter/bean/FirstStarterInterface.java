fba91df6a2bb9390055efd62e28554da09b449c5
Elmax19 <maksim.kohnovich@mail.ru>
2022-03-07 11:58:11 +0300

    making project build correctly

---
+++ commons-starter/src/main/java/by/elmax19/commonsstarter/bean/FirstStarterInterface.java
@@ -0,0 +1,4 @@
+package by.elmax19.commonsstarter.bean;
+
+public interface FirstStarterInterface {
+}
---
+++ volleyball-info-module/src/main/java/by/elmax19/volleyballinfomodule/config/AppConfig.java
@@ -0,0 +1,13 @@
+package by.elmax19.volleyballinfomodule.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+//    @Bean
+//    public FirstStarterInterface firstInterfaceImpl() {
+//        return new FirstInterfaceImpl();
+//    }
+
+}
