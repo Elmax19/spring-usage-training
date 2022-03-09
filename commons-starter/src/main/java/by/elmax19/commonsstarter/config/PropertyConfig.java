28724615ce12fc7fe5e59f0d63e38921dbceb490
Elmax19 <maksim.kohnovich@mail.ru>
2022-03-09 10:07:41 +0300

    adding PropertyBean

---
+++ commons-starter/src/main/java/by/elmax19/commonsstarter/config/PropertyConfig.java
@@ -0,0 +1,7 @@
+package by.elmax19.commonsstarter.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties
+public class PropertyConfig {
+}
