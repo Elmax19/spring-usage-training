0d9c9373052e37107b3e9cc46705e2ef45e064f6
Elmax19 <maksim.kohnovich@mail.ru>
2022-03-16 10:06:12 +0300

    code refactoring

--- src/main/java/by/elmax19/app/service/DatabaseService.java
+++ src/main/java/by/elmax19/app/service/DatabaseService.java
@@ -1,22 +1,24 @@
 package by.elmax19.app.service;
 
-import com.mongodb.client.MongoClient;
+import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.DependsOn;
 import org.springframework.stereotype.Component;
 
 @Component
-@DependsOn("mongoClient")
 public class DatabaseService {
     private final MongoDatabase database;
 
-    public DatabaseService(@Autowired MongoClient mongoClient, @Value("${mongodb.database.name}") String databaseName) {
-        database = mongoClient.getDatabase(databaseName);
+    public DatabaseService(
+            @Value("${spring.data.mongodb.host}") String host,
+            @Value("${spring.data.mongodb.port}") int port,
+            @Value("${mongodb.database.name}") String databaseName) {
+        MongoClient client = new MongoClient(host, port);
+        database = client.getDatabase(databaseName);
     }
+
 
     public MongoCollection<Document> getCollection(String collectionName) {
         return database.getCollection(collectionName);
