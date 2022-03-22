daf81588c46175e58df19edf7696552d339e1b98
Elmax19 <maksim.kohnovich@mail.ru>
2022-03-22 13:00:07 +0300

    some refactoring

--- src/test/java/by/elmax19/app/PlayerIntegrationTests.java
+++ src/test/java/by/elmax19/app/PlayerIntegrationTests.java
@@ -22,20 +22,16 @@ public class PlayerIntegrationTests {
     private static PlayerRepository playerRepo;
     @Autowired
     private PlayerRepository playerRepoBean;
-    private final Player firstPlayer;
-    private Player secondPlayer;
-    private Player thirdPlayer;
-    private Player fourthPlayer;
-    private Player fifthPlayer;
-    private Player sixthPlayer;
 
     @PostConstruct
     private void init() {
         playerRepo = playerRepoBean;
     }
 
-    public PlayerIntegrationTests() {
-        firstPlayer = Player.builder()
+    @Test
+    @DisplayName("First player has been added")
+    void checkPlayerCreation() {
+        Player player = Player.builder()
                 .id(new ObjectId())
                 .surname("Rezende")
                 .name("Bruno")
@@ -48,7 +44,15 @@ public class PlayerIntegrationTests {
                 .number(1)
                 .nationalities(List.of("Brazilian"))
                 .build();
-        secondPlayer = Player.builder()
+        long countOfDocumentBeforeCreation = playerRepo.count();
+        playerRepo.save(player);
+        assertEquals(countOfDocumentBeforeCreation + 1, playerRepo.count());
+    }
+
+    @Test
+    @DisplayName("Second player has been deleted")
+    void checkPlayerRemoval() {
+        Player player = playerRepo.save(Player.builder()
                 .id(new ObjectId())
                 .surname("Mikhaylov")
                 .name("Maxim")
@@ -60,8 +64,16 @@ public class PlayerIntegrationTests {
                 .currentClub("Zenit Kazan")
                 .number(18)
                 .nationalities(List.of("Russian"))
-                .build();
-        thirdPlayer = Player.builder()
+                .build());
+        long countOfDocumentBeforeRemoval = playerRepo.count();
+        playerRepo.delete(player);
+        assertEquals(countOfDocumentBeforeRemoval - 1, playerRepo.count());
+    }
+
+    @Test
+    @DisplayName("Third player has been found by id")
+    void checkPlayerFindingById() {
+        Player player = playerRepo.save(Player.builder()
                 .id(new ObjectId())
                 .surname("Wilfredo")
                 .name("Leon")
@@ -73,21 +85,36 @@ public class PlayerIntegrationTests {
                 .currentClub("Sir Safety Perugia")
                 .number(9)
                 .nationalities(List.of("Cuban", "Polish"))
-                .build();
-        fourthPlayer = Player.builder()
+                .build());
+        Optional<Player> foundedPlayer = playerRepo.findById(player.getId());
+        assertTrue(foundedPlayer.isPresent());
+        assertEquals(player, foundedPlayer.get());
+    }
+
+    @Test
+    @DisplayName("Second player has been found by player Criteria")
+    void checkPlayerFindingByCriteria() {
+        Player player = playerRepo.save(Player.builder()
                 .id(new ObjectId())
-                .surname("Robertlandy")
-                .name("Simon")
-                .age(34)
-                .height(2.08)
-                .spike(387)
-                .block(350)
-                .position(Position.MIDDLE_BLOCKER)
-                .currentClub("Cucine Lube Civitanova")
-                .number(13)
-                .nationalities(List.of("Cuban"))
-                .build();
-        fifthPlayer = Player.builder()
+                .surname("Mikhaylov")
+                .name("Maxim")
+                .age(33)
+                .height(2.02)
+                .spike(360)
+                .block(340)
+                .position(Position.OPPOSITE_HITTER)
+                .currentClub("Zenit Kazan")
+                .number(18)
+                .nationalities(List.of("Russian"))
+                .build());
+        Player foundedPlayer = playerRepo.findPlayerBySurnameAndName(player.getSurname(), player.getName());
+        assertEquals(player, foundedPlayer);
+    }
+
+    @Test
+    @DisplayName("Fifth and sixth players have been found by current club Criteria")
+    void checkPlayersFindingAllByCriteria() {
+        Player firstPlayer = playerRepo.save(Player.builder()
                 .id(new ObjectId())
                 .surname("Abdel-Aziz")
                 .name("Nimir")
@@ -99,8 +126,8 @@ public class PlayerIntegrationTests {
                 .currentClub("Modena Volley")
                 .number(14)
                 .nationalities(List.of("Dutch"))
-                .build();
-        sixthPlayer = Player.builder()
+                .build());
+        Player secondPlayer = playerRepo.save(Player.builder()
                 .id(new ObjectId())
                 .surname("N'Gapeth")
                 .name("Earvin")
@@ -112,63 +139,33 @@ public class PlayerIntegrationTests {
                 .currentClub("Modena Volley")
                 .number(9)
                 .nationalities(List.of("French"))
-                .build();
-    }
-
-    @Test
-    @DisplayName("First player has been added")
-    void checkPlayerCreation() {
-        long countOfDocumentBeforeCreation = playerRepo.count();
-        playerRepo.save(firstPlayer);
-        assertEquals(countOfDocumentBeforeCreation + 1, playerRepo.count());
-    }
-
-    @Test
-    @DisplayName("Second player has been deleted")
-    void checkPlayerRemoval() {
-        secondPlayer = playerRepo.save(secondPlayer);
-        long countOfDocumentBeforeRemoval = playerRepo.count();
-        playerRepo.delete(secondPlayer);
-        assertEquals(countOfDocumentBeforeRemoval - 1, playerRepo.count());
-    }
-
-    @Test
-    @DisplayName("Third player has been found by id")
-    void checkPlayerFindingById() {
-        thirdPlayer = playerRepo.save(thirdPlayer);
-        playerRepo.findAll().forEach(player -> System.out.println(player.toString()));
-        Optional<Player> foundedPlayer = playerRepo.findById(thirdPlayer.getId());
-        assertTrue(foundedPlayer.isPresent());
-        assertEquals(thirdPlayer, foundedPlayer.get());
-    }
-
-    @Test
-    @DisplayName("Second player has been found by player Criteria")
-    void checkPlayerFindingByCriteria() {
-        secondPlayer = playerRepo.save(secondPlayer);
-        Player foundedPlayer = playerRepo.findPlayerBySurnameAndName(secondPlayer.getSurname(), secondPlayer.getName());
-        assertEquals(secondPlayer, foundedPlayer);
-    }
-
-    @Test
-    @DisplayName("Fifth and sixth players have been found by current club Criteria")
-    void checkPlayersFindingAllByCriteria() {
-        fifthPlayer = playerRepo.save(fifthPlayer);
-        sixthPlayer = playerRepo.save(sixthPlayer);
-        List<Player> players = playerRepo.findPlayersByCurrentClub(fifthPlayer.getCurrentClub());
+                .build());
+        List<Player> players = playerRepo.findPlayersByCurrentClub(firstPlayer.getCurrentClub());
         assertEquals(2, players.size());
-        assertTrue(players.contains(fifthPlayer) && players.contains(sixthPlayer));
+        assertTrue(players.contains(firstPlayer) && players.contains(secondPlayer));
     }
 
     @Test
     @DisplayName("Fourth player has been updated")
     void checkPlayerUpdating() {
-        fourthPlayer = playerRepo.save(fourthPlayer);
-        fourthPlayer.setAge(32);
-        playerRepo.save(fourthPlayer);
-        Optional<Player> foundedPlayer = playerRepo.findById(fourthPlayer.getId());
+        Player player = playerRepo.save(Player.builder()
+                .id(new ObjectId())
+                .surname("Robertlandy")
+                .name("Simon")
+                .age(34)
+                .height(2.08)
+                .spike(387)
+                .block(350)
+                .position(Position.MIDDLE_BLOCKER)
+                .currentClub("Cucine Lube Civitanova")
+                .number(13)
+                .nationalities(List.of("Cuban"))
+                .build());
+        player.setAge(32);
+        playerRepo.save(player);
+        Optional<Player> foundedPlayer = playerRepo.findById(player.getId());
         assertTrue(foundedPlayer.isPresent());
-        assertEquals(fourthPlayer, foundedPlayer.get());
+        assertEquals(player, foundedPlayer.get());
     }
 
     @AfterAll
