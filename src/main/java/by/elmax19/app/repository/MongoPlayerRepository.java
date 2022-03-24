409e740d2c36b2febb84ea1371a1e17aeec18a79
Elmax19 <maksim.kohnovich@mail.ru>
2022-03-24 17:26:13 +0300

    code cleanup

--- src/main/java/by/elmax19/app/repository/MongoPlayerRepository.java
+++ src/main/java/by/elmax19/app/repository/MongoPlayerRepository.java
@@ -4,6 +4,5 @@ import by.elmax19.app.model.mongo.MongoPlayer;
 import org.bson.types.ObjectId;
 import org.springframework.data.repository.CrudRepository;
 
-
 public interface MongoPlayerRepository extends CrudRepository<MongoPlayer, ObjectId> {
 }
